ead1e12e1e17195f39e901aa29b59b18159f63dd
staleycyn <45440075+staleycyn@users.noreply.github.com>
2024-02-23 07:22:59 -0800

    Delete Allfiles/Labs/08/az104-08-install_IIS.ps1

--- Allfiles/Labs/08/az104-08-install_IIS.ps1
+++
@@ -1,3 +0,0 @@
-powershell.exe Install-WindowsFeature -name Web-Server -IncludeManagementTools
-powershell.exe Remove-Item -Path 'C:\inetpub\wwwroot\iisstart.htm'
-powershell.exe Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value "$env:computername"
